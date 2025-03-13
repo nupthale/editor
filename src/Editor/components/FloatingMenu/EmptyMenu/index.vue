5fdfb031f0f036933b66a9abbf1b140455b16d24
hale <hale@stocke.com.cn>
2025-03-13 16:34:52 +0800

    chore: save

--- src/Editor/components/FloatingMenu/EmptyMenu/index.vue
+++ src/Editor/components/FloatingMenu/EmptyMenu/index.vue
@@ -64,9 +64,9 @@ export default defineComponent({
                         </div>
                     </MenuItem>
                     <MenuItem key="6" class="!w-full !p-1 !m-0 !h-auto !min-h-auto !leading-none !rounded-[4px]">
-                        <div class="flex items-center">
+                        <div class="flex items-center highlightBlock">
                             <span class="mr-4 inline-flex items-center justify-center w-[24px] h-[24px]">
-                                <LucideIcon icon={TextQuote} width={20}></LucideIcon>
+                                <LucideIcon icon={TextQuote} width={20} color="#994bff"></LucideIcon>
                             </span>
                             高亮块
                         </div>
@@ -79,4 +79,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.highlightBlock {
+    background-image: linear-gradient(135deg, #994bff 10%, #36cde1 90%);
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    font-weight: 500;
+}
 </style>
