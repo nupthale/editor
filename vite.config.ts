caddfe843986cb10db32b11b746d2fd1052cfd7b
hale <hale@stocke.com.cn>
2025-04-10 10:23:07 +0800

    chore: save

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/editor",
   plugins: [vue(), vueJsx()],
   build: {
     outDir: 'docs'
